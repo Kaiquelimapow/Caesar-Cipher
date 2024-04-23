5788a400ccbf0173b158b11336187cdc326917e3
KaiqueLimapow <kaiquelima1011@gmail.com>
2024-04-23 12:27:03 -0300

    adiciona rota em code

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def caesar_cipher(name=None):
+def homw(name=None):
         return render_template('index.html', name=name)
+
+@app.route("/encode")
+def encode(name=None):
+        return render_template('encode.html', name=name)
