03685834b9e7370d921cd791545a2366a5e9bcf3
KaiqueLimapow <kaiquelima1011@gmail.com>
2024-05-16 12:23:12 -0300

    ENCODE E DECODE

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 def create_app():
     #create and configure the app
     app = Flask(__name__)
+    app.config ['SECRET_KEY'] = 'kakakakakakkakakkakaa'
 
     #A simple page that says hello
     @app.route("/")
     def home (name='Home'):
         return render_template('index.html', name=name)
 
-    @app.route("/encode")
+    @app.route("/encode",methods=('GET', 'POST'))
     def encode (name= 'encode'):
-        return render_template('encode.html', name=name) 
+        if request.method == 'POST':
+            cipher_key = request.form['cipher_key']
+            message = request.form['message']
+        
+            if not cipher_key:
+                flash('A chave é obrigatória...')
+            elif not message:
+                flash('A mensagem é necessária...')
+            else:
+                flash("ENCODE")
+
+        return render_template('encode.html', name=name)
+
     return app
