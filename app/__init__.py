35a89dea01c867f181ff0b61d727625fdae7f824
KaiqueLimapow <kaiquelima1011@gmail.com>
2024-05-21 11:58:00 -0300

    aula de hoje

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash
+from app.encode import encode_message
 
 def create_app():
     #create and configure the app
@@ -13,11 +14,19 @@ def create_app():
     @app.route("/encode",methods=('GET', 'POST'))
     def encode (name= 'encode'):
         if request.method == 'POST':
+            try:
+                cipher_key = int(request.form['cypher_key'])
+            except:
+                cypher_key = 0
+
+            message = request.form['message']
             cipher_key = request.form['cipher_key']
             message = request.form['message']
         
-            if not cipher_key:
+            if not cipher_key == 0:
                 flash('A chave é obrigatória...')
+            elif not cipher_key:
+                flash('a chave é obrigatória...')
             elif not message:
                 flash('A mensagem é necessária...')
             else:
